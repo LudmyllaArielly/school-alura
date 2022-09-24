dde024b05cdbf3586c08a3bd58470163e0900e12
Ludmylla <ludmay4@gmail.com>
2022-09-24 13:24:59 -0300

    cria findByCode no repositório section

--- src/main/java/br/com/alura/school/section/SectionRepository.java
+++ src/main/java/br/com/alura/school/section/SectionRepository.java
@@ -2,5 +2,8 @@ package br.com.alura.school.section;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface SectionRepository extends JpaRepository<Section, Long> {
+    Optional<Section> findByCode(String code);
 }
