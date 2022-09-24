fc34bdddb9c522bdf280864bb1cfc310f9dd25fd
Ludmylla <ludmay4@gmail.com>
2022-09-24 13:03:07 -0300

    cria repositório para video

---
+++ src/main/java/br/com/alura/school/video/VideoRepository.java
@@ -0,0 +1,9 @@
+package br.com.alura.school.video;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface VideoRepository extends JpaRepository<Video, Long> {
+    Optional<Video> findByVideo(String video);
+}
