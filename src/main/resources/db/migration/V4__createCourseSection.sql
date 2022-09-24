5a0383e3eb92a0e0bcc491c8c6ce3163c03c74fa
Ludmylla <ludmay4@gmail.com>
2022-09-23 21:44:14 -0300

    remove virgula da coluna section_id

--- src/main/resources/db/migration/V4__createCourseSection.sql
+++ src/main/resources/db/migration/V4__createCourseSection.sql
@@ -1,10 +1,10 @@
-CREATE TABLE Course_section (
+CREATE TABLE course_section (
     course_id BIGINT NOT NULL,
-    section_id BIGINT NOT NULL,
+    section_id BIGINT NOT NULL
 );
 
-ALTER TABLE Course_section ADD CONSTRAINT fk_course_id
+ALTER TABLE course_section ADD CONSTRAINT fk_course_id
 FOREIGN KEY (course_id) REFERENCES course(id);
 
-ALTER TABLE Course_section ADD CONSTRAINT fk_section_id
+ALTER TABLE course_section ADD CONSTRAINT fk_section_id
 FOREIGN KEY (section_id) REFERENCES `section`(id);
