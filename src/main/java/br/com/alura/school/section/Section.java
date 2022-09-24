fa32f59c5981d78b5258fb343d80e963f99813eb
Ludmylla <ludmay4@gmail.com>
2022-09-24 13:18:53 -0300

    adiciona video na entidade section

--- src/main/java/br/com/alura/school/section/Section.java
+++ src/main/java/br/com/alura/school/section/Section.java
@@ -1,11 +1,13 @@
 package br.com.alura.school.section;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import br.com.alura.school.video.Video;
+
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static javax.persistence.GenerationType.IDENTITY;
 
@@ -29,6 +31,13 @@ public class Section {
     @Column(nullable = false)
     private String author;
 
+    @ManyToMany(cascade=CascadeType.ALL)
+    @JoinTable(name = "section_video",
+            joinColumns = @JoinColumn(name = "section_id"),
+            inverseJoinColumns = @JoinColumn(name = "video_id"))
+    private List<Video> videos = new ArrayList<>();
+
+
     public Section() {
     }
 
@@ -48,5 +57,13 @@ public class Section {
 
     public String getAuthor() {
         return author;
+    }
+
+    public List<Video> getVideos() {
+        return videos;
+    }
+
+    public boolean addVideo(Video video){
+        return getVideos().add(video);
     }
 }
