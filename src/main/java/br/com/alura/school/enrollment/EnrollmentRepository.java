7652bd27709e32f831639f896aa921ce0365e052
Ludmylla <ludmay4@gmail.com>
2022-09-25 12:00:27 -0300

    adiciona findByUser e findCourseById no repositório de enrollment

--- src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
+++ src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
@@ -1,9 +1,13 @@
 package br.com.alura.school.enrollment;
 
+import br.com.alura.school.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
     Optional<Enrollment> findByCourseIdAndUserId(Long courseId, Long userId);
+    List<Enrollment> findByUser(User user);
+    Optional<Enrollment> findCourseById(Long courseId);
 }
